2c6f4631b66ff2fb1e9839f9b1f6c26e4560f3e6
Dan Haywood <dan@haywood-associates.co.uk>
2015-08-02 13:39:36 +0100

    For stricter checking per ISIS-1182

--- integtests/src/test/java/org/isisaddons/module/publishing/integtests/PublishingEventSerializerModuleSystemInitializer.java
+++ integtests/src/test/java/org/isisaddons/module/publishing/integtests/PublishingEventSerializerModuleSystemInitializer.java
@@ -46,20 +46,12 @@ public class PublishingEventSerializerModuleSystemInitializer {
             with(new DataNucleusPersistenceMechanismInstaller());
 
             // services annotated with @DomainService
-            withServicesIn( "org.isisaddons.module.publishing"
-                            ,"org.apache.isis.core.wrapper"
-                            ,"org.apache.isis.applib"
-                            ,"org.apache.isis.core.metamodel.services"
-                            ,"org.apache.isis.core.runtime.services"
-                            ,"org.apache.isis.objectstore.jdo.datanucleus.service.support" // IsisJdoSupportImpl
-                            ,"org.apache.isis.objectstore.jdo.datanucleus.service.eventbus" // EventBusServiceJdo
-                            );
+            withServicesIn( "org.isisaddons.module.publishing");
         }
 
         private static IsisConfiguration testConfiguration() {
             final IsisConfigurationForJdoIntegTests testConfiguration = new IsisConfigurationForJdoIntegTests();
-            testConfiguration.addRegisterEntitiesPackagePrefix("org.isisaddons.module.publishing.dom");
-            testConfiguration.addRegisterEntitiesPackagePrefix("org.isisaddons.module.publishing.fixture.dom");
+            testConfiguration.addRegisterEntitiesPackagePrefix("org.isisaddons.module.publishing");
             return testConfiguration;
         }
     }
